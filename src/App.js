b9fc9390c3714346720c636f0f959f500da6b5ba
Hiz <kljuyt107@gmail.com>
2020-04-22 01:54:13 +0900

    :sparkles: Update app.js and index.js in front

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+import PeoplePage from "./pages/PeoplePage";
+import LunchGroup from "./pages/LunchGroupPage";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="title">Let's make Random Lunch Groups!</h1>
+      <div className="container">
+        <PeoplePage />
+        <LunchGroup />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
